27a3485e709ba6e76fb40a54b3d4d1bc7ba14e15
G <99215533+gsec0@users.noreply.github.com>
2026-02-09 21:06:39 +0000

    v1.3 scheduling + advanced settings page

--- Garage_Security.ino
+++ Garage_Security.ino
@@ -9,7 +9,14 @@
  * 
  * v1.2 18/06/2020 01:00
  * - Added a getTime() function to receive the time when a client connects to the server.
- * - Added a calcTime() function to constantly keep track of time downto -1min accuracy.
+ * - Added a calcTime() function to constantly keep track of time downto -1sec accuracy.
+ * 
+ * v1.3 18/06/2020 22:00
+ * - Bug fixes
+ * - Added a working automatic mode with a time scheduler.
+ * - Added mode selection buttons to the website *need to finish*
+ * - Changed accelerometer operating mode to accel-only, wake-up cycle of 5Hz
+ * - During OFF or off-schedule (AUTO) modes the accelerometer goes to sleep
  */
 
 #include <ESP8266HTTPClient.h>
@@ -19,13 +26,16 @@
 #include <MPU6050.h>
 #include <Wire.h>
 #include "index.h"
+#include "settings.h"
+#include "message.h"
+#include <EEPROM_Rotate.h>
 
 MPU6050 accel;
 #define SDA 0
 #define SCL 2
 
 #define MA_SS 8
-#define accDelay 75
+#define accDelay 200
 #define httpDelay 10000
 #define theInit 0
 #define theAlert 1
@@ -33,6 +43,8 @@ MPU6050 accel;
 
 const char* yourSSID = "admin";
 const char* yourPASS = "pass";
+const char* defaultSSID = "ESP_Garage";
+const char* defaultPASS = "NotSecure";
 
 String eventInit = "ESP_Initialised";
 String eventAlert = "ESP_GarageDoor";
@@ -43,19 +55,27 @@ String IFTTT_KEY = "";
 String IFTTT_URL;
 
 int16_t ax, ay, az;
+bool firstTime = false;
+bool autoMode;
+bool startSet;
+bool endSet;
+bool sysState;
 bool timeInc = true;
 byte minCount;
 byte hrCount;
-byte errorCheck = 0;
+byte timeStart[2];
+byte timeEnd[2];
+unsigned long secCount;
 float angle;
+byte angle_thresh;
 long angle_i = 0;
 bool trigger_set;
 bool trigger_pull;
+bool master_trigger;
 unsigned long timer;
 unsigned long accTimer;
 unsigned long httpTimer;
 unsigned long timeTimer;
-unsigned long timeKeeper;
 int httpCode;
 int offset[6] = {-3057, -777, 1335, 109, -10, -54};
 // OFFSET VALUES: -3057ax -777ay 1335az 109gx -10gy -54gz
@@ -71,6 +91,7 @@ long az_f;
 
 HTTPClient http;
 ESP8266WebServer server(80);
+EEPROM_Rotate EEPROMr;
 
 void setup() {
   /* ----- I2C ----- */
@@ -79,6 +100,9 @@ void setup() {
 
   Serial.begin(115200);
   Serial.println();
+
+  EEPROMr.size(4);
+  EEPROMr.begin(4096);
 
   /* ----- WiFi ----- */
   Serial.print("Connecting to local network");
@@ -90,99 +114,146 @@ void setup() {
     delay(500);
     Serial.print(".");
     if (((unsigned long)millis() - timer) > 10000) {
+      firstTime = true;
       Serial.println();
       Serial.println("Connection timed out!\n");
-      makeRequest(theError,"WiFi");
-      while(true) delay(0);
+      Serial.println("Setting up a first-time AP...");
+      WiFi.mode(WIFI_AP);
+      WiFi.softAP(defaultSSID, defaultPASS);
     }
   }
   Serial.println();
-  Serial.println("Connected!");Serial.print("IP: ");Serial.println(WiFi.localIP());
-  errorCheck |= B00000001;
+  if (!firstTime) { Serial.println("Connected!");Serial.print("IP: ");Serial.println(WiFi.localIP()); }
+  else {
+    Serial.print("AP IP: ");
+    Serial.println(WiFi.softAPIP());
+  }
 
   /* ----- Server ----- */
   delay(1000);
   Serial.println();
-  server.on("/",[](){server.send_P(200,"text/html",webpage);});
-  server.on("/toggle",toggle);
-  server.on("/update",updateWeb);
-  server.on("/time",getTime);
+  if (!firstTime) {
+    server.on("/",[](){server.send_P(200,"text/html",webpage);});
+    server.on("/settings",[](){server.send_P(200,"text/html",settings);});
+    server.on("/submit",handleForm);
+    server.on("/toggle",toggle);
+    server.on("/update",updateWeb);
+    server.on("/getMode",sendMode);
+    server.on("/getSched",sendSched);
+    server.on("/giveMode",receiveMode);
+    server.on("/time",getTime);
+  } else {
+    server.on("/",[](){server.send_P(200,"text/html",settings);});
+  }
   Serial.println("Setting up a server...");
   server.begin();
-  errorCheck |= B00000010;
-
+  
   /* ----- Accelerometer init ----- */
+  if (firstTime) return;
   delay(1000);
+  accel.reset();
+  delay(250);
   Serial.println();
-  Serial.println("Initiliasing the accelerometer.");
+  Serial.println("Resetting and initialising the accel...\n");
   accel.initialize();
-
-  // verify connection
+  setOffset();
   if (accel.testConnection()) {
     Serial.println("I2C: OK!");
-    errorCheck |= B00000100;
   } else {
     Serial.println("I2C: ERROR! Check the I2C bus.\n");
-    makeRequest(theError,"I2C Test");
-    while (true) delay(0);
+    makeRequest(theError,"I2C: ERROR! Check the I2C bus.");
+    ESP.reset();
   }
-
-  /* ----- Offsets ----- */
-  delay(1000);
-  Serial.println();
-  setOffset();
 
   /* ----- Accelerometer zeroing ----- */
   delay(1000);
   Serial.println();
   Serial.print("Filling buffer");
   for (byte i=0; i<255; i++) {
-    accel.getAcceleration(&ax, &ay, &az);
-    MAF();
-    if (i%26 == 0) Serial.print(".");
+    getAccel();
+    if (i%85 == 0) Serial.print(".");
     delay(1);
   }
   Serial.println();
   delay(250);
   Serial.print("Zeroing the accelerometer");
   for (int i=0; i<1000; i++) {
-    accel.getAcceleration(&ax, &ay, &az);
-    MAF();
+    getAccel();
     calcAngle();
     angle_i += angle;
-    if (i%100 == 0) Serial.print(".");
+    if (i%333 == 0) Serial.print(".");
     delay(1);
   }
   angle_i /= 1000;
-  errorCheck |= B00001000;
+  wakeMPU();
+  //accel.setSleepEnabled(true);
+  accel.setSleepEnabled(false);
   Serial.println("\n");
 
-  /* ----- Init error check ----- */
-  if (errorCheck == B00001111) {
-    makeRequest(theInit,"");
-    Serial.println();
-    Serial.println("System Ready!");
-    Serial.println();
-  } else {
-    Serial.println("The system could not initialise!\n");
-    makeRequest(theError, "initilisation");
-    while (true) delay(0);
-  }
+  /* ----- Inititialisation complete ----- */
+  makeRequest(theInit,"");
+  Serial.println();
+  Serial.println("System Ready!");
+  Serial.println();
   
   accTimer = httpTimer = millis();
   trigger_set = false;
+  master_trigger = false;
+  startSet = false;
+  endSet = false;
+  angle_thresh = 15;
+  
+  autoMode = 1;
+  sysState = 0;
+  timeStart[0] = 20;
+  timeStart[1] = 0;
+  timeEnd[0] = 7;
+  timeEnd[1] = 0;
 }
 
 void loop() {
   server.handleClient();
+  if (firstTime) return;
   calcTime();
+  /*
+  if (autoMode == 1) {
+    if (minCount == timeStart[1] && hrCount == timeStart[0] && !startSet) {
+      wakeMPU();
+      sysState = 1;
+      startSet = true;
+      endSet = false;
+      Serial.println("Schedule started.\n");
+    } else if (minCount == timeEnd[1] && hrCount == timeEnd[0] && !endSet) {
+      accel.setSleepEnabled(true);
+      sysState = 0;
+      startSet = false;
+      endSet = true;
+      Serial.println("Schedule ended.\n");
+    }
+  }
+  */
+  if (autoMode == 1) {
+    if (minCount == timeStart[1] && hrCount == timeStart[0] && !startSet) {
+      wakeMPU();
+      sysState = 1;
+      startSet = true;
+      endSet = false;
+      Serial.println("Schedule started.\n");
+    } else if (minCount == timeEnd[1] && hrCount == timeEnd[0] && !endSet) {
+      accel.setSleepEnabled(true);
+      sysState = 0;
+      startSet = false;
+      endSet = true;
+      Serial.println("Schedule ended.\n");
+    }
+  }
   
-  if (((unsigned long)millis() - accTimer) > accDelay) {
-    accel.getAcceleration(&ax, &ay, &az);
-    MAF();
+  if ((((unsigned long)millis() - accTimer) > accDelay) && sysState == 1) {
+    getAccel();
     calcAngle();
     angle -= angle_i;
-    if (angle > 20) trigger_set = true;
+    if (angle > angle_thresh && !master_trigger) trigger_set = true;
+    else if (angle < angle_thresh) master_trigger = false;
     accTimer = millis();
   }
 
@@ -193,7 +264,8 @@ void loop() {
   }
 }
 
-void MAF() {
+void getAccel() {
+  accel.getAcceleration(&ax, &ay, &az);
   // Moving avg filter
   if (buff < MA_SS) {
     ax_buff[buff] = ax;
@@ -224,18 +296,26 @@ void setOffset() {
   accel.setXAccelOffset(offset[0]);
   accel.setYAccelOffset(offset[1]);
   accel.setZAccelOffset(offset[2]);
-  Serial.println("Sensor offsets are set!");
+  Serial.println("Sensor offsets are set!\n");
 }
 
 void toggle() {
   trigger_set = false;
+  master_trigger = true;
   Serial.println("Standing by...");
   Serial.println();
 }
 
 void updateWeb() {
-  if (!trigger_set) server.send(200,"text/plain","STANDBY");
-  else server.send(200,"text/plain","INTRUDER!");
+  if (!trigger_set) {
+    if (autoMode) {
+      if (startSet) server.send(200,"text/plain","AUTO ON-SCHEDULE");
+      else server.send(200,"text/plain","AUTO OFF-SCHEDULE");
+    } else {
+      if (sysState) server.send(200,"text/plain","MANUAL ON");
+      else server.send(200,"text/plain","MANUAL OFF");
+    }
+  } else server.send(200,"text/plain","INTRUDER!");
 }
 
 void makeRequest(byte event, String payload) {
@@ -256,11 +336,13 @@ void makeRequest(byte event, String payload) {
 }
 
 void getTime() {
-  static String payload;
-  payload = server.arg("plain");
-  hrCount = payload.toInt() / 100;
-  minCount = payload.toInt() % 100;
-  Serial.print("The time is: ");Serial.println(hrCount*100+minCount);
+  hrCount = server.arg("hour").toInt();
+  minCount = server.arg("minute").toInt();
+  secCount = server.arg("second").toInt();
+  Serial.println("Time synchronised!\n");
+  Serial.print("The time is: ");
+  Serial.println((String)hrCount+":"+(String)minCount+":"+(String)secCount);
+  secCount *= 1000;
   timeInc = true;
 }
 
@@ -269,41 +351,125 @@ void calcTime() {
     timeTimer = millis();
     timeInc = false;
   }
-  if (((unsigned long)millis() - timeTimer) >= 60000) {
+  if (((unsigned long)millis() - timeTimer) >= (60000-secCount)) {
+    secCount = 0;
     minCount++;
-    if (minCount == 60) {
+    if (minCount >= 60) {
       minCount = 0;
       hrCount++;
-      hrCount = (hrCount == 24?0:hrCount);
+      hrCount = (hrCount >= 24?0:hrCount);
     }
-    Serial.print("The time is: ");Serial.println(hrCount*100+minCount);
+    Serial.println((String)hrCount+":"+(String)minCount);
     timeInc = true;
   }
 }
-/*
-void loadCredentials() {
-  EEPROM.begin(512);
-  EEPROM.get(0, yourSSID);
-  EEPROM.get(0+sizeof(yourSSID), yourPASS);
-  char ok[2+1];
-  EEPROM.get(0+sizeof(yourSSID)+sizeof(yourPASS), ok);
-  EEPROM.end();
-  if (String(ok) != String("OK")) {
-    ssid[0] = 0;
-    password[0] = 0;
-  }
-  Serial.println("Recovered credentials:");
-  Serial.println(yourSSID);
-  Serial.println(strlen(yourPASS)>0?"********":"<no password>");
+
+void wakeMPU() { // low-power 5Hz accelerometer-only mode.
+  accel.setWakeCycleEnabled(true);
+  accel.setWakeFrequency(1);
+  accel.setSleepEnabled(false);
+  accel.setTempSensorEnabled(false);
+  accel.setStandbyXGyroEnabled(true);
+  accel.setStandbyYGyroEnabled(true);
+  accel.setStandbyZGyroEnabled(true);
 }
 
-void saveCredentials() {
-  EEPROM.begin(512);
-  EEPROM.put(0, yourSSID);
-  EEPROM.put(0+sizeof(yourSSID), yourPASS);
-  char ok[2+1] = "OK";
-  EEPROM.put(0+sizeof(yourSSID)+sizeof(yourPASS), ok);
-  EEPROM.commit();
-  EEPROM.end();
+void handleForm() {
+  static String ssid;
+  static String pass;
+  static String startTime;
+  static String endTime;
+  static int delimiter;
+
+  Serial.println("Form submitted!");
+
+  ssid = server.arg("SSID");
+  pass = server.arg("PASS");
+  startTime = server.arg("startTime");
+  endTime = server.arg("endTime");
+  
+  if (startTime != "") {
+    delimiter = startTime.indexOf(':');
+    timeStart[0] = startTime.substring(0,delimiter).toInt();
+    timeStart[1] = startTime.substring(delimiter+1).toInt();
+  }
+  if (endTime != "") {
+    delimiter = endTime.indexOf(':');
+    timeEnd[0] = endTime.substring(0,delimiter).toInt();
+    timeEnd[1] = endTime.substring(delimiter+1).toInt();
+  }
+  Serial.println("Schedule start: "+(String)timeStart[0]+":"+(String)timeStart[1]);
+  Serial.println("Schedule end: "+(String)timeEnd[0]+":"+(String)timeEnd[1]);
+  server.send_P(200,"text/html",message);
+
+  // if ssid > 0 && pass > 0 then reset device
 }
-*/
+
+void sendMode() {
+  Serial.println("Mode sent!");
+  if (autoMode == 1) server.send(200,"text/plain","2");
+  else if (sysState == 1) server.send(200,"text/plain","1");
+  else server.send(200,"text/plain","0");
+}
+
+void sendSched() {
+  Serial.println("Sched sent!");
+  server.send(200,"text/plain",(String)timeStart[0]+":"+(String)timeStart[1]+"/"+(String)timeEnd[0]+":"+(String)timeEnd[1]);
+}
+
+void receiveMode() {
+  static String newmode;
+  newmode = server.arg("theMode");
+  if (newmode.toInt() < 2) {
+    autoMode = 0;
+    sysState = newmode.toInt();
+  } else {
+    autoMode = 1;
+    sysState = 0;
+  }
+  Serial.println("Mode updated!");
+}
+
+/* write to eeprom where mystr is char[]
+ * for(int i=0 ; i < sizeof(mystr) ; i++)
+  {
+    EEPROM.write( i,mystr[i] );
+  }
+  EEPROM.write(sizeof(mystr)+1,'/0')
+  EEPROM.commit();
+
+
+  read from eeprom
+  index = 0;
+  this = EEPROM.read(address+offset);
+  
+
+  add so that on first boot the device sends an 'OK/0' (if not present) to EEPROM and writes 0 to SSID and PASS and 21->7 sched
+  then on second boot, read from the offset "OK"[2+1] until /0 is found. that data is SSID, then read from the offset + length of SSID + 1 and so on
+  data is sorted as such: SSID[32+1], PASS[32+1], timeStart[0][1+1], timeStart[1][1+1], timeEnd[0][1+1], timeEnd[1][1+1]. [=74+3]
+  add char limits on website
+ */
+
+void writeEEPROMdefaults() {
+  for (byte i=0; i<sizeof(yourSSID); i++) {
+    EEPROMr.write(3+i, yourSSID[i]);
+  }
+  for (byte i=0; i<sizeof(yourPASS); i++) {
+    EEPROMr.write(3+sizeof(yourSSID)+i, yourPASS[i]);
+  }
+  Serial.println("Defaults Written!");
+}
+
+char[] readEEPROM() {
+  static char okCheck[2+1];
+  static char ssid[];
+  static char pass[];
+  okCheck[0] = EEPROMr.read(0);
+  okCheck[1] = EEPROMr.read(1);
+  if (okCheck != "ok") {
+    writeEEPROM(0,"ok");
+    writeEEPROMdefaults();
+  } else {
+    
+  }
+}
